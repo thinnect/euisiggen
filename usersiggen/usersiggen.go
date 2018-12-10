69ac20d93c94f429b41a1c33b51832e96547b6ec
Raido Pahtma <raidopahtma@gmail.com>
2018-12-10 17:13:30 +0200

    Possibly makes final versions of v3 now.

--- usersiggen/usersiggen.go
+++ usersiggen/usersiggen.go
@@ -23,6 +23,11 @@ var g_version_major uint8 = 3
 var g_version_minor uint8 = 0
 var g_version_patch uint8 = 0
 
+const SIGNATURE_TYPE_EUI64 = 0     // EUI64 is the IEEE Extended Unique Identifier
+const SIGNATURE_TYPE_BOARD = 1     // Boards are the core of the system - MCU
+const SIGNATURE_TYPE_PLATFORM = 2  // Platforms define the set of components
+const SIGNATURE_TYPE_COMPONENT = 3 // Components list individual parts of a platform
+
 type UserSignature struct {
 }
 
@@ -34,9 +39,9 @@ type EUISignature struct {
 	signature_size uint16
 	signature_type uint8
 
-	unix_time int64 // nx_int64_t unix_time; // Same moment in time
+	unix_time int64 // Signature generation time
 
-	eui64 uint64
+	eui64 uint64 // EUI-64, byte array
 
 	// crc uint16
 }
@@ -49,16 +54,21 @@ type ComponentSignature struct {
 	signature_size uint16
 	signature_type uint8
 
-	unix_time int64 // nx_int64_t unix_time; // Same moment in time
+	unix_time int64 // Signature generation time
 
-	name [16]byte //char boardname[16]; // up to 16 chars or 0 terminated
+	component_uuid [16]byte
+	name           [16]byte //char boardname[16]; // up to 16 chars or 0 terminated
 
 	version_major    uint8 // nx_uint8_t pcb_version_major;
 	version_minor    uint8 // nx_uint8_t pcb_version_minor;
 	version_assembly uint8 // nx_uint8_t pcb_version_assembly;
 
-	component_uuid    [16]byte
+	serial_number [16]byte // Possibly an UUID, but could be a \0 terminated string
+
 	manufacturer_uuid [16]byte
+
+	data_length uint16
+	// data     []byte  // compoent specific data - calibration etc
 
 	// crc uint16
 }
@@ -70,7 +80,7 @@ func (self *UserSignature) ConstructEUISignature(t time.Time, eui64 uint64) (*EU
 	sig.sig_version_patch = g_version_patch
 
 	sig.signature_size = uint16(binary.Size(sig)) + 2
-	sig.signature_type = 0
+	sig.signature_type = SIGNATURE_TYPE_EUI64
 	sig.eui64 = eui64
 
 	sig.unix_time = t.Unix()
@@ -78,8 +88,11 @@ func (self *UserSignature) ConstructEUISignature(t time.Time, eui64 uint64) (*EU
 	return sig, nil
 }
 
-func (self *UserSignature) ConstructComponentSignature(t time.Time, boardname string, boardversion BoardVersion, boarduuid string, manufuuid string, signature_type uint8) (*ComponentSignature, error) {
-	var err error
+func (self *UserSignature) ConstructComponentSignature(t time.Time, boardname string,
+	boardversion BoardVersion, uuid [16]byte, manufuuid [16]byte,
+	serial [16]byte,
+	signature_type uint8) (*ComponentSignature, error) {
+
 	sig := new(ComponentSignature)
 	sig.sig_version_major = g_version_major
 	sig.sig_version_minor = g_version_minor
@@ -103,15 +116,11 @@ func (self *UserSignature) ConstructComponentSignature(t time.Time, boardname st
 	sig.version_minor = boardversion.minor
 	sig.version_assembly = boardversion.assembly
 
-	sig.component_uuid, err = uuid.FromString(boarduuid)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("Board UUID error(%d)", err))
-	}
+	sig.component_uuid = uuid
 
-	sig.manufacturer_uuid, err = uuid.FromString(manufuuid)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("Manufacturer UUID error(%d)", err))
-	}
+	sig.serial_number = serial
+
+	sig.manufacturer_uuid = manufuuid
 
 	return sig, nil
 }
@@ -148,8 +157,8 @@ func (self *ComponentSignature) BoardVersion() string {
 	return fmt.Sprintf("%d.%d.%d", self.version_major, self.version_minor, self.version_assembly)
 }
 
-func (self *ComponentSignature) TimestampString() string {
-	return "" //fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d", self.year, self.month, self.day, self.hours, self.minutes, self.seconds)
+func TimestampString(t time.Time) string {
+	return fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
 
 type BoardVersion struct {
@@ -302,38 +311,50 @@ func markEui(infile string, esig EUISignature, csig ComponentSignature) error {
 }
 
 func printGeneratorVersion() {
-	fmt.Printf("User Signature Area generator %d.%d.%d\n", g_version_major, g_version_minor, g_version_patch)
+	fmt.Printf("Device signature generator %d.%d.%d\n", g_version_major, g_version_minor, g_version_patch)
 }
 
 func main() {
 	var opts struct {
-		Boardname   string       `long:"boardname" required:"true" description:"The name of the PCB that the user signature will be used for."`
-		Version     BoardVersion `long:"version" required:"true" description:"The version of the board X.Y.Z."`
-		Board_uuid  string       `long:"boarduuid" required:"true" description:"Board UUID. 16 bytes"`
-		Manuf_uuid  string       `long:"manuuid" required:"true" description:"Manufacturer UUID. 16 bytes"`
-		Output      string       `long:"out" default:"sigdata.bin" description:"The output file name."`
-		Sigdir      string       `long:"sigdir" default:"sigdata" description:"Where to store EUI_XXXXXXXXXXXXXXXX.bin files."`
-		Euifile     string       `long:"euifile" default:"eui.txt" description:"The file containing available EUIs."`
-		Sign_type   uint8        `long:"type" required:"true" description:"Signature type."`
-		Timestamp   int64        `long:"timestamp" required:"false" description:"Use the specified timestamp."`
-		ShowVersion func()       `short:"V" description:"Show generator version."`
-		Debug       bool         `long:"debug" description:"The file containing available EUIs."`
+		Type string `long:"type" required:"true" description:"Signature type - board, platform, component"`
+
+		Name         string       `long:"name"         required:"true" description:"The name of the component that the user signature will be used for."`
+		Version      BoardVersion `long:"version"      required:"true" description:"The version of the board X.Y.Z."`
+		UUID         string       `long:"uuid"         required:"true" description:"Board/Platform/Component UUID. 16 bytes."`
+		Manufacturer string       `long:"manufacturer" required:"true" description:"Manufacturer UUID. 16 bytes."`
+
+		Serial     string `long:"serial"     required:"false" description:"Serial number, string format. Up to 16 characters."`
+		SerialUUID string `long:"serialuuid" required:"false" description:"Serial number, UUID format. 16 bytes."`
+
+		Eui     string `long:"eui"     default:""        description:"Do not retrieve EUI from euifile, override with the specified EUI."`
+		Euifile string `long:"euifile" default:"eui.txt" description:"The file containing available EUIs."`
+		Sigdir  string `long:"sigdir"  default:"sigdata" description:"Where to store EUI_XXXXXXXXXXXXXXXX.bin files."`
+
+		Timestamp int64 `long:"timestamp" required:"false" description:"Use the specified timestamp."`
+
+		Output string `long:"out" default:"sigdata.bin" description:"The output file name."`
+
+		ShowVersion func() `short:"V" description:"Show generator version."`
+		Debug       bool   `long:"debug" description:"The file containing available EUIs."`
 	}
 
+	var gen UserSignature
 	var eui uint64
+	var err error
+	var sigdata []byte
 
 	opts.ShowVersion = func() {
 		printGeneratorVersion()
 		os.Exit(0)
 	}
 
-	_, err := flags.Parse(&opts)
+	_, err = flags.Parse(&opts)
 	if err != nil {
 		fmt.Printf("ERROR parsing arguments\n")
 		os.Exit(1)
 	}
 
-	if _, err := os.Stat(opts.Sigdir); os.IsNotExist(err) {
+	if _, err = os.Stat(opts.Sigdir); os.IsNotExist(err) {
 		err = os.Mkdir(opts.Sigdir, 0770)
 		if err != nil {
 			fmt.Printf("ERROR creating output directory: %s\n", err)
@@ -341,23 +362,6 @@ func main() {
 		}
 	}
 
-	if opts.Sign_type == 0 {
-		eui, err = getEui(opts.Euifile)
-		if err != nil {
-			fmt.Printf("ERROR getting EUI64: %s\n", err)
-			os.Exit(1)
-		}
-	} else {
-		eui = 0
-	}
-
-	sigfile := filepath.Join(opts.Sigdir, fmt.Sprintf("EUI-64_%016X.bin", eui))
-	if _, err := os.Stat(sigfile); err == nil {
-		fmt.Printf("ERROR generating sigdata: signature file for %016X exists at %s\n", eui, sigfile)
-		os.Exit(1)
-	}
-
-	var gen UserSignature
 	var timestamp time.Time
 	if opts.Timestamp > 0 {
 		timestamp = time.Unix(opts.Timestamp, 0).UTC()
@@ -365,69 +369,192 @@ func main() {
 		timestamp = time.Now().UTC()
 	}
 
-	var esig *EUISignature
-	esig, err = gen.ConstructEUISignature(timestamp, eui)
+	var component_uuid [16]byte
+	component_uuid, err = uuid.FromString(opts.UUID)
 	if err != nil {
-		fmt.Printf("ERROR generating sigdata: %s\n", err)
+		fmt.Printf("UUID error(%d)", err)
 		os.Exit(1)
 	}
 
-	var csig *ComponentSignature
-	csig, err = gen.ConstructComponentSignature(timestamp, opts.Boardname, opts.Version, opts.Board_uuid, opts.Manuf_uuid, 1)
+	var manufacturer_uuid [16]byte
+	manufacturer_uuid, err = uuid.FromString(opts.Manufacturer)
 	if err != nil {
-		fmt.Printf("ERROR generating sigdata: %s\n", err)
+		fmt.Printf("Manufacturer UUID error(%d)", err)
 		os.Exit(1)
 	}
 
-	esigdata, err := gen.Serialize(esig)
-	if err != nil {
-		fmt.Printf("ERROR generating sigdata: %s\n", err)
-		os.Exit(1)
-	}
-
-	csigdata, err := gen.Serialize(csig)
-	if err != nil {
-		fmt.Printf("ERROR generating sigdata: %s\n", err)
-		os.Exit(1)
-	}
-
-	sigdata := append(esigdata, csigdata...)
-
-	err = ioutil.WriteFile(sigfile, sigdata, 0440)
-	if err != nil {
-		fmt.Printf("ERROR writing output file: %s\n", err)
-		os.Exit(1)
-	}
-
-	if opts.Sign_type == 0 {
-		err = markEui(opts.Euifile, *esig, *csig)
+	var serial [16]byte
+	if len(opts.SerialUUID) > 0 {
+		serial, err = uuid.FromString(opts.UUID)
 		if err != nil {
-			fmt.Printf("ERROR marking %016X in %s: %s\n", eui, opts.Euifile, err)
+			fmt.Printf("Serial UUID error(%d)", err)
 			os.Exit(1)
 		}
-	}
-
-	err = ioutil.WriteFile(opts.Output, sigdata, 0640)
-	if err != nil {
-		fmt.Printf("ERROR writing output file: %s\n", err)
+	} else if len(opts.Serial) > 0 {
+		if len(opts.Serial) > 16 {
+			fmt.Printf("Serial number string too long, max 16 characters.")
+			os.Exit(1)
+		}
+		copy(serial[:], opts.Serial)
+	} else {
+		fmt.Printf("ERRPR: No serial number.")
 		os.Exit(1)
 	}
 
-	fmt.Printf("EUI-64: %016X\n", esig.eui64)
+	if opts.Type == "board" {
+		overrideEui := false
+		if len(opts.Eui) > 0 {
+			if len(opts.Eui) != 16 {
+				fmt.Printf("ERROR specified override EUI64 '%s' is not suitable!", opts.Eui)
+				os.Exit(1)
+			}
+			overrideEui = true
+			eui, err = parseEui(opts.Eui)
+			if err != nil {
+				fmt.Printf("ERROR parsing EUI64: %s\n", err)
+				os.Exit(1)
+			}
+		} else {
+			eui, err = getEui(opts.Euifile)
+			if err != nil {
+				fmt.Printf("ERROR getting EUI64: %s\n", err)
+				os.Exit(1)
+			}
+		}
+
+		sigfile := filepath.Join(opts.Sigdir, fmt.Sprintf("EUI-64_%016X.bin", eui))
+		if overrideEui == false {
+			if _, err := os.Stat(sigfile); err == nil {
+				fmt.Printf("ERROR generating sigdata: signature file for %016X exists at %s\n", eui, sigfile)
+				os.Exit(1)
+			}
+		}
+
+		esig, err := gen.ConstructEUISignature(timestamp, eui)
+		if err != nil {
+			fmt.Printf("ERROR generating sigdata: %s\n", err)
+			os.Exit(1)
+		}
+
+		csig, err := gen.ConstructComponentSignature(timestamp, opts.Name, opts.Version, component_uuid, manufacturer_uuid, serial, SIGNATURE_TYPE_BOARD)
+		if err != nil {
+			fmt.Printf("ERROR generating sigdata: %s\n", err)
+			os.Exit(1)
+		}
+
+		esigdata, err := gen.Serialize(esig)
+		if err != nil {
+			fmt.Printf("ERROR generating sigdata: %s\n", err)
+			os.Exit(1)
+		}
+
+		csigdata, err := gen.Serialize(csig)
+		if err != nil {
+			fmt.Printf("ERROR generating sigdata: %s\n", err)
+			os.Exit(1)
+		}
+
+		if overrideEui == false {
+			if err := markEui(opts.Euifile, *esig, *csig); err != nil {
+				fmt.Printf("ERROR marking %016X in %s: %s\n", eui, opts.Euifile, err)
+				os.Exit(1)
+			}
+		}
+
+		sigdata = append(esigdata, csigdata...)
+
+		if _, err := os.Stat(sigfile); err == nil {
+			bakfile := sigfile + ".bak"
+			if _, err := os.Stat(bakfile); err == nil {
+				fmt.Printf("ERROR generating sigdata: signature file at %s and a backup file at %s exists for %016X\n", sigfile, bakfile, eui)
+				os.Exit(1)
+			}
+			err := os.Rename(sigfile, bakfile)
+			if err != nil {
+				fmt.Printf("ERROR generating sigdata: creating backup file for %016X at %s failed: %s\n", eui, bakfile, err)
+				os.Exit(1)
+			}
+		}
+
+		if err := ioutil.WriteFile(sigfile, sigdata, 0440); err != nil {
+			fmt.Printf("ERROR writing output file: %s\n", err)
+			os.Exit(1)
+		}
+
+		if err := ioutil.WriteFile(opts.Output, sigdata, 0440); err != nil {
+			fmt.Printf("ERROR writing output file: %s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("EUI-64: %016X\n", eui)
+
+	} else if opts.Type == "platform" || opts.Type == "component" {
+		var tp uint8
+		if opts.Type == "platform" {
+			tp = SIGNATURE_TYPE_PLATFORM
+		} else if opts.Type == "component" {
+			tp = SIGNATURE_TYPE_COMPONENT
+		}
+
+		if _, err := os.Stat(opts.Output); os.IsNotExist(err) {
+			fmt.Printf("ERROR initial signature file %s not found!", opts.Output)
+			os.Exit(1)
+		}
+
+		csig, err := gen.ConstructComponentSignature(timestamp, opts.Name, opts.Version, component_uuid, manufacturer_uuid, serial, tp)
+		if err != nil {
+			fmt.Printf("ERROR generating sigdata: %s\n", err)
+			os.Exit(1)
+		}
+
+		csigdata, err := gen.Serialize(csig)
+		if err != nil {
+			fmt.Printf("ERROR generating sigdata: %s\n", err)
+			os.Exit(1)
+		}
+
+		f, err := os.OpenFile(opts.Output, os.O_APPEND|os.O_WRONLY, 0440)
+		if err != nil {
+			fmt.Printf("ERROR opening output file: %s\n", err)
+			os.Exit(1)
+		}
+		if _, err := f.Write(csigdata); err != nil {
+			fmt.Printf("ERROR writing output file: %s\n", err)
+			os.Exit(1)
+		}
+		if err := f.Close(); err != nil {
+			fmt.Printf("ERROR closing output file: %s\n", err)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("%s is not a known signature type, supported types are: board, platform and component.")
+		os.Exit(1)
+	}
+
 	if opts.Debug {
 		printGeneratorVersion()
-		fmt.Printf("Timestamp: %d (%s)\n", esig.unix_time, "")
-		//esig.TimestampString())
-		fmt.Printf("Boardname: %s\n", opts.Boardname)
-		fmt.Printf("Version:   %s\n", opts.Version)
-		fmt.Printf("Output:    %s\n", opts.Output)
-		fmt.Printf("Sigdir:    %s\n", opts.Sigdir)
-		fmt.Printf("Euifile:   %s\n", opts.Euifile)
+		fmt.Printf("Timestamp:    %d (%s)\n", timestamp.UTC().Unix(), TimestampString(timestamp.UTC()))
+		fmt.Printf("Name:         %s\n", opts.Name)
+		fmt.Printf("Version:      %s\n", opts.Version)
+		if len(opts.SerialUUID) > 0 {
+			uus, _ := uuid.FromBytes(serial[:])
+			fmt.Printf("Serial:       %s\n", uus)
+		} else {
+			fmt.Printf("Serial:       %s\n", serial)
+		}
+		uuc, _ := uuid.FromBytes(component_uuid[:])
+		fmt.Printf("UUID:         %s\n", uuc)
+		uum, _ := uuid.FromBytes(manufacturer_uuid[:])
+		fmt.Printf("Manufacturer: %s\n", uum)
 
-		fmt.Printf("SIG(%d):\n", len(sigdata))
-		fmt.Printf("%X\n", sigdata[0:256])
-		fmt.Printf("%X\n", sigdata[256:512])
-		fmt.Printf("%X\n", sigdata[512:768])
+		fmt.Printf("Output:       %s\n", opts.Output)
+		fmt.Printf("Sigdir:       %s\n", opts.Sigdir)
+		fmt.Printf("Euifile:      %s\n", opts.Euifile)
+
+		//fmt.Printf("SIG(%d):\n", len(sigdata))
+		//fmt.Printf("%X\n", sigdata[0:256])
+		//fmt.Printf("%X\n", sigdata[256:512])
+		//fmt.Printf("%X\n", sigdata[512:768])
 	}
 
 }
